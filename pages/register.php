523a147dac34f2b0ac62d432a4a8857fb006781b
NicolaeSalagor <B00124666@mytudublin.ie>
2022-03-09 17:32:15 +0000

    1

--- pages/register.php
+++ pages/register.php
@@ -29,31 +29,31 @@ $insert = $model->insert();
         <section class="register-section">
             <h1 class="register-title">Register</h1>
             <form action="" method="post">
-                <div class="form-group">
-                    <label for="">firstname</label>
-                    <input type="text" name="firstname" class="form-control">
+                <div class="form-group firstname-section">
+                    <h2 class="firstname">Firstname</h2>
+                    <input type="text" name="firstname" class="form-control firstnameInput">
                 </div>
-                <div class="form-group">
-                    <label for="">lastname</label>
-                    <input type="text" name="lastname" class="form-control">
+                <div class="form-group lastname-section">
+                    <h2 class="lastname">Lastname</h2>
+                    <input type="text" name="lastname" class="form-control lastnameInput">
                 </div>
-                <div class="form-group">
-                    <label for="">username</label>
-                    <input type="text" name="username" class="form-control">
+                <div class="form-group username-section">
+                    <h2 class="username">Username</h2>
+                    <input type="text" name="username" class="form-control usernameInput">
                 </div>
-                <div class="form-group">
-                    <label for="">password</label>
-                    <input type="text" name="password" class="form-control">
-                </div>
-
-                <div class="form-group">
-                    <label for="">email</label>
-                    <input type="text" name="email" class="form-control">
+                <div class="form-group password-section">
+                    <h2 class="password">Password</h2>
+                    <input type="text" name="password" class="form-control passwordInput">
                 </div>
 
-                <div class="form-group">
-                    <a href="" class="reset-btn" type="reset" class="button" id="reset_button">Reset</a>
-                    <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+                <div class="form-group email-section">
+                    <h2 class="email">Email</h2>
+                    <input type="text" name="email" class="form-control emailInput">
+                </div>
+
+                <div class="form-group buttons">
+                    <button type="reset" class="button reset-btn" id="reset_button">Reset</button>
+                    <button type="submit" class="submit-btn" name="submit" >Submit</button>
                 </div>
             </form>
         </section>
